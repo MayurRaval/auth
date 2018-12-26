c5c63db3f65972c877a4b69b94b120e04cc1ec50
Mayur Raval <mayurraval@asite.com>
2018-12-26 10:28:31 +0530

    authentication and navigation to logout

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React,{ Component } from 'react';
 import { View , Text } from 'react-native';
 import firebase from '@firebase/app'
 import '@firebase/auth'
-import { Header } from './component/common'
+import { Header, Button, Spinner } from './component/common'
 import LoginForm from './component/LoginForm';
+
+
 class App extends Component {
+
+  state = { loggedIn: null };
 
   componentWillMount(){
      firebase.initializeApp({
@@ -15,13 +19,38 @@ class App extends Component {
         storageBucket: 'authentication-eb11b.appspot.com',
         messagingSenderId: '943590187524'
      });
+     firebase.auth().onAuthStateChanged((user) =>{
+        if (user) {
+          this.setState({ loggedIn : true })
+        } else {
+          this.setState({ loggedIn : false })
+        }
+     });
+  }
+
+  renderContent(){
+    switch(this.state.loggedIn){
+      case true:
+          return(
+            <Button>
+              Log Out
+            </Button>
+          );
+        break;
+      case false:
+          return <LoginForm />;
+        break;
+      default:
+          return <Spinner size="large"/>;
+        break;
+    }
   }
 
   render(){
     return(
         <View>
           <Header headerText="Authentication"/>
-          <LoginForm/>
+          {this.renderContent()}
         </View>
     );
   };
--- src/component/common/Input.js
+++ src/component/common/Input.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+  const { inputStyle, labelStyle, containerStyle} = styles;
   return(
-    <View>
-      <Text>{label}</Text>
+    <View style={containerStyle}>
+      <Text style={labelStyle}>{label}</Text>
       <TextInput
+        secureTextEntry={secureTextEntry}
+        placeholder={placeholder}
+        autoCorrect={false}
+        style={inputStyle}
         value= {value}
         onChangeText= {onChangeText}
-        style= {{height: 20,width: 100}}
         />
     </View>
   );
@@ -19,7 +23,7 @@ const styles = {
     color: '#000',
     paddingRight: 5,
     paddingLeft: 5,
-    fintSize: 18,
+    fontSize: 18,
     lineHeight: 23,
     flex: 2
   },
@@ -32,7 +36,7 @@ const styles = {
     height: 40,
     flex: 1,
     flexDirection:'row',
-    align:'center'
+    alignItems:'center'
   }
 }
 
